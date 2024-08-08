5ce86b8161f7379fd8b0b2e89146b62e50732333
martin0123478 <martin_agui97@outlook.es>
2024-08-07 21:26:39 -0600

    feat:add budget tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-
-
+import { useMemo } from "react"
+import { useBudget } from "./Hooks/useBudget"
 import { FormBudget } from "./components/FormBudget"
+import { BudgetTracker } from "./components/BudgetTracker"
 
 
 
 function App() {
 
-
+  const { state } = useBudget()
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
 
   return (
     <>
@@ -18,7 +20,8 @@ function App() {
 
       </header>
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <FormBudget />
+        {isValidBudget ? <BudgetTracker /> : <FormBudget />}
+
       </div>
     </>
   )
