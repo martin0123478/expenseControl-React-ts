8fcd21fae7c02dcd48ca97a48681dbb11f3b98db
martin0123478 <martin_agui97@outlook.es>
2024-08-22 17:36:59 -0600

    feat:add circular-progress-bar

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,15 +1,25 @@
-import { useMemo } from "react"
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import { useBudget } from "../Hooks/useBudget"
 import { AmountDisplay } from "./AmountDisplay"
+import 'react-circular-progressbar/dist/styles.css'
 
 
 export const BudgetTracker = () => {
     const { state, totalExpenses, expensesRemaining } = useBudget()
+    const pecentage = +((totalExpenses / state.budget) * 100).toFixed(2)
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap:5">
             <div className="flex justify-center">
-                <img src="/grafico.jpg" alt="grafica" />
+                <CircularProgressbar
+                    value={pecentage}
+                    styles={buildStyles({
+                        pathColor: '#3b82f6',
+                        trailColor: '#F5F5F5',
+                        textSize: 8, textColor: '#3b82f6'
+                    })}
+                    text={`${pecentage}% Gastado`}
+                />
             </div>
             <div className="flex flex-col justify-center items-center gap-8">
                 <button type="button" className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg">
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,3 +1,4 @@
+
 import { categories } from "../data/categories"
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css'
